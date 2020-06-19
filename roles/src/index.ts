0966cd68bff5782f3308f53f9ee67e0846148b8e
Brian Singer <brian.c.singer@gmail.com>
2020-06-19 12:57:47 +0200

    test

--- roles/src/index.ts
+++ roles/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-    console.log('rolling with the roles service!!');
+    console.log('rolling with the roles service!');
 
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
