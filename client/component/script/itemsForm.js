a01cdea703c231fd4c2f62dbd00779a51a1b4e09
Brian Singer <brian.c.singer@gmail.com>
2020-06-24 22:25:57 +0200

    updating script detail

---
+++ client/component/script/itemsForm.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+
+export default ({ onSave }) => {
+    const [value, setValue] = useState('');
+    const [type, setType] = useState('');
+    const [editing, setEditing] = useState(false);
+
+    const handleAddQuestionItem = () => handleAddItem('question');
+    const handleAddTextItem = () => handleAddItem('text');
+    const handleAddItem = (itemType) => {
+        setType(itemType);
+        setEditing(true);
+    };
+
+    const handleSaveItem = (e) => {
+        if (onSave) {
+            onSave({ type, value });
+        }
+        setType('');
+        setEditing(false);
+        setValue('');
+    };
+
+    const handleCancleClick = () => {
+        setType('');
+        setEditing(false);
+        setValue('');
+    };
+
+    return (
+        <div>
+            {editing && (
+                <>
+                    <div className="form-group">
+                        <textarea
+                            value={value}
+                            rows="3"
+                            onChange={(e) => setValue(e.target.value)}
+                            className="form-control"
+                        />
+                    </div>
+                    <div>
+                        <button
+                            className="btn btn-primary mr-2"
+                            onClick={handleSaveItem}
+                        >
+                            Save
+                        </button>
+                        <button
+                            className="btn mr-2"
+                            onClick={handleCancleClick}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </>
+            )}
+            {!editing && (
+                <div>
+                    <button
+                        className="btn btn-primary mr-2"
+                        onClick={handleAddQuestionItem}
+                    >
+                        + Question
+                    </button>
+                    <button
+                        className="btn btn-primary mr-2"
+                        onClick={handleAddTextItem}
+                    >
+                        + Text
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
---
+++ client/component/script/scriptItems.js
@@ -0,0 +1,25 @@
+export default ({ items = [] }) => {
+    const renderLinkItem = (item, index) => (
+        <p>
+            <a href={item.url} target="_blank" referrer="self">
+                {item.url}
+            </a>
+        </p>
+    );
+
+    const renderItem = (item, index) => {
+        switch (item.type) {
+            case 'question':
+            case 'text':
+                return <p>{item.text}</p>;
+            default:
+                return;
+        }
+    };
+
+    const itemsList = items.map((item, index) => (
+        <div key={index}>{renderItem(item, index)}</div>
+    ));
+
+    return <div>{itemsList}</div>;
+};
