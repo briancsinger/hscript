46651f9d649f5b7abb3adc789e79a1eb4ace1773
Brian Singer <brian.c.singer@gmail.com>
2020-06-25 21:07:22 +0200

    list roles that the user is an editor of

--- roles/src/routes/roles/__test__/list.test.ts
+++ roles/src/routes/roles/__test__/list.test.ts
@@ -23,7 +23,7 @@ it('can only be accessed if the user is signed in', async () => {
     return await request(app).get('/api/roles').expect(401);
 });
 
-it('returns roles for current user', async () => {
+it('returns roles created by current user', async () => {
     const notMeUserId = mongoose.Types.ObjectId().toHexString();
     const notMyRole = await buildRole({ createdBy: notMeUserId });
 
@@ -41,6 +41,28 @@ it('returns roles for current user', async () => {
 
     expect(response.body[0].id).toEqual(myRole1.id);
     expect(response.body[1].id).toEqual(myRole2.id);
+});
+
+it('returns roles that the current user is an editor on', async () => {
+    const notMeUserId = mongoose.Types.ObjectId().toHexString();
+    const meUserId = mongoose.Types.ObjectId().toHexString();
+    const myRole1 = await buildRole({ createdBy: meUserId });
+    const myRole2 = await buildRole({ createdBy: meUserId });
+    const myRole3 = await buildRole({ createdBy: notMeUserId });
+    myRole3.editors.push(meUserId);
+    await myRole3.save();
+
+    const cookie = global.signin(meUserId);
+    const response = await request(app)
+        .get('/api/roles')
+        .set('Cookie', cookie)
+        .expect(200);
+
+    expect(response.body.length).toEqual(3);
+
+    expect(response.body[0].id).toEqual(myRole1.id);
+    expect(response.body[1].id).toEqual(myRole2.id);
+    expect(response.body[2].id).toEqual(myRole3.id);
 });
 
 it('returns empty array if no roles exist for this user', async () => {
