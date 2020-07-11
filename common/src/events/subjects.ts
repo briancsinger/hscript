7ff0178cc5a145fa37060951aa0d924e756601b1
Brian Singer <brian.c.singer@gmail.com>
2020-07-11 11:09:59 -0400

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -8,6 +8,8 @@ export enum Subjects {
     OrganizationCreated = 'organization:created',
 
     RoleCreated = 'role:created',
+    ScriptCreated = 'script:created',
+    ScriptUpdated = 'script:updated',
 
     OrderCanceled = 'order:canceled',
 
---
+++ common/src/events/types/script-item.ts
@@ -0,0 +1,4 @@
+export enum ScriptItemType {
+    Text = 'text',
+    Question = 'question',
+}
