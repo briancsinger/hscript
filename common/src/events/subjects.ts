f6aee61a2a8cf0f1aa91cd77582e42b2247b6376
Brian Singer <brian.c.singer@gmail.com>
2020-07-12 16:06:30 -0400

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,17 +1,8 @@
 export enum Subjects {
-    ExpirationComplete = 'expiration:complete',
-
-    TicketCreated = 'ticket:created',
-    TicketUpdated = 'ticket:updated',
-
     UserCreated = 'user:created',
     OrganizationCreated = 'organization:created',
 
     RoleCreated = 'role:created',
     ScriptCreated = 'script:created',
     ScriptUpdated = 'script:updated',
-
-    OrderCanceled = 'order:canceled',
-
-    PaymentCreated = 'payment:created',
 }
