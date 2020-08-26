5783f7be3ff2a6d40a5405d7fb7708d5474dfdec
Brian Singer <brian.c.singer@gmail.com>
2020-08-26 15:35:22 +0200

    update build-client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,10 +3,20 @@ import axios from 'axios';
 export default ({ req }) => {
     if (typeof window === 'undefined') {
         return axios.create({
-            baseURL:
-                'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.singer.land',
             headers: req.headers,
         });
     }
     return axios.create({ baseURL: '/' });
 };
+
+// export default ({ req }) => {
+//     if (typeof window === 'undefined') {
+//         return axios.create({
+//             baseURL:
+//                 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+//             headers: req.headers,
+//         });
+//     }
+//     return axios.create({ baseURL: '/' });
+// };
