6b3a7cb9abc533fb40d0ad49310d751c00b635d2
Brian Singer <brian.c.singer@gmail.com>
2020-08-26 13:55:21 +0200

    material-ui (#23)

--- client/component/header.js
+++ client/component/header.js
@@ -1,6 +1,32 @@
 import Link from 'next/link';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+
+// import IconButton from '@material-ui/core/IconButton'
+// import MenuIcon from '@material-ui/icons/Menu'
+// import Toolbar from '@material-ui/core/Toolbar'
+// import Typography from '@material-ui/core/Typography'
+
+const ButtonLink = ({ className, href, hrefAs, children, prefetch }) => (
+    <Link href={href} as={hrefAs} prefetch>
+        <a className={className}>{children}</a>
+    </Link>
+);
+
+const useStyles = makeStyles((theme) => ({
+    grow: {
+        flexGrow: 1,
+    },
+}));
 
 export default ({ currentUser }) => {
+    const classes = useStyles();
+
     const links = [
         currentUser && { label: 'Create Role', href: '/roles/new' },
         !currentUser && { label: 'Sign up', href: '/auth/signup' },
@@ -10,29 +36,25 @@ export default ({ currentUser }) => {
         .filter((x) => x)
         .map(({ label, href }) => {
             return (
-                <li key={href} className="nav-item">
-                    <Link href={href}>
-                        <a className="nav-link">{label}</a>
-                    </Link>
-                </li>
+                <Button color="inherit" component={ButtonLink} href={href}>
+                    {label}
+                </Button>
             );
         });
 
     return (
-        <nav className="navbar navbar-light bg-light">
-            <Link href="/">
-                <a className="navbrand">hscript</a>
-            </Link>
-            <div className="d-flex justify-content-end">
-                <ul className="nav d-flex align-items-center">
-                    {currentUser && (
-                        <li key="email" className="nav-item">
-                            {currentUser.email}
-                        </li>
-                    )}
-                    {links}
-                </ul>
-            </div>
-        </nav>
+        <AppBar position="fixed" color="primary">
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Button color="inherit" component={ButtonLink} href={'/'}>
+                    hscript
+                </Button>
+                <div className={classes.grow} />
+                {currentUser && <Typography>{currentUser.email}</Typography>}
+                {links}
+            </Toolbar>
+        </AppBar>
     );
 };
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import 'fontsource-roboto';
 
+import Head from 'next/head';
 import buildClient from '../api/build-client';
 import Header from '../component/header';
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
         <div>
+            <Head>
+                <title>hscript</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
             <Header currentUser={currentUser} />
             <div className="container">
                 <Component currentUser={currentUser} {...pageProps} />
