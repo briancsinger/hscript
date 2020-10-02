45390ba2913e33076bb639a40cda1db25a1cbe52
Brian Singer <brian.c.singer@gmail.com>
2020-10-02 10:43:13 +0200

    Update skills component

--- client/component/role/skillListItem.js
+++ client/component/role/skillListItem.js
@@ -1,10 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import clsx from 'clsx';
 import { useDrag, useDrop } from 'react-dnd';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import DragHandleIcon from '@material-ui/icons/DragHandle';
+import DeleteIcon from '@material-ui/icons/Delete';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import { makeStyles } from '@material-ui/core';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Divider from '@material-ui/core/Divider';
+import { makeStyles, Button, Typography, IconButton } from '@material-ui/core';
 
 InputAdornment;
 const SKILL_TYPE = 'SKILL_TYPE';
@@ -16,6 +22,52 @@ const useStyles = makeStyles((theme) => ({
             cursor: 'grab',
         },
     },
+    dragIconWrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    listItemWrapper: {
+        [theme.breakpoints.up('xs')]: {
+            paddingTop: theme.spacing(1),
+            paddingBottom: theme.spacing(1),
+            paddingRight: theme.spacing(2),
+        },
+        [theme.breakpoints.up('sm')]: {
+            paddingTop: theme.spacing(2),
+            paddingBottom: theme.spacing(2),
+            paddingRight: theme.spacing(4),
+        },
+        // [theme.breakpoints.up('md')]: {
+        //     paddingTop: theme.spacing(2),
+        //     paddingBottom: theme.spacing(2),
+        //     paddingRight: theme.spacing(4),
+        // },
+        '&:hover': {
+            background: 'rgba(250,250,250,1)',
+            cursor: 'text',
+        },
+        '&:hover .MuiButtonBase-root': {
+            opacity: 1,
+        },
+    },
+    listItemWrapperEditing: {
+        '&:hover': {
+            background: 'initial',
+        },
+    },
+    listItemButton: {
+        transition: 'opacity 1s',
+    },
+    listItemDeleteButton: {
+        opacity: 0,
+        position: 'absolute',
+        right: '10px',
+        top: '10px',
+        transition: 'opacity .2s',
+    },
+    listItemText: {
+        whiteSpace: 'break-spaces',
+    },
 }));
 
 const SkillListItem = ({
@@ -24,9 +76,11 @@ const SkillListItem = ({
     index,
     moveSkillListItem,
     handleSkillChange,
+    onDeleteSkill,
 }) => {
     const ref = useRef(null);
     const classes = useStyles();
+    const [isEditing, setIsEditing] = useState(false);
 
     const [, drop] = useDrop({
         accept: SKILL_TYPE,
@@ -86,24 +140,85 @@ const SkillListItem = ({
 
     drag(drop(ref));
 
+    const handleDeleteClicked = (e) => {
+        e.preventDefault();
+        if (onDeleteSkill) {
+            console.log(skill._id);
+            onDeleteSkill(skill._id);
+        }
+    };
+
+    const handleItemClick = (e) => {
+        e.preventDefault();
+        console.log('1111');
+        setIsEditing(true);
+    };
+
+    const handleCancelClicked = (e) => {
+        e.preventDefault();
+        console.log('asd');
+        setIsEditing(false);
+    };
+
     return (
-        <Grid item ref={ref} style={{ opacity }}>
-            <TextField
-                multiline
-                fullWidth
-                variant="outlined"
-                placeholder={placeholder}
-                value={skill.text}
-                onChange={handleSkillChange.bind(this, skill)}
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <DragHandleIcon className={classes.dragIcon} />
-                        </InputAdornment>
-                    ),
-                }}
-            />
-        </Grid>
+        <ListItem
+            className={clsx(
+                classes.listItemWrapper,
+                isEditing && classes.listItemWrapperEditing,
+            )}
+            key={skill._id}
+            ref={ref}
+            style={{ opacity }}
+        >
+            <ListItemAvatar className={classes.dragIconWrapper}>
+                <DragHandleIcon className={classes.dragIcon} />
+            </ListItemAvatar>
+            <ListItemText>
+                {(isEditing && (
+                    <Grid container spacing={1} direction="column">
+                        <Grid item>
+                            <TextField
+                                multiline
+                                fullWidth
+                                variant="outlined"
+                                placeholder={placeholder}
+                                value={skill.text}
+                                onChange={handleSkillChange.bind(this, skill)}
+                            />
+                        </Grid>
+                        <Grid item container spacing={1}>
+                            <Grid item>
+                                <Button variant="contained" color="secondary">
+                                    Update
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button onClick={handleCancelClicked}>
+                                    Cancel
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                )) || (
+                    <>
+                        <Typography
+                            variant="body1"
+                            onClick={handleItemClick}
+                            className={classes.listItemText}
+                        >
+                            {skill.text}
+                        </Typography>
+                        <IconButton
+                            aria-label="delete"
+                            className={classes.listItemDeleteButton}
+                            onClick={handleDeleteClicked}
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                    </>
+                )}
+            </ListItemText>
+        </ListItem>
     );
 };
 
